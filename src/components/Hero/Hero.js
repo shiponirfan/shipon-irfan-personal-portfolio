9aaade9247354bfdd8270f8a9d80905b2ec2119d
shiponirfan <imshiponirfan@gmail.com>
2023-12-11 16:58:54 +0600

    Animated Text Added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import heroBg from "@/assets/images/hero-bg.jpg";
 import heroImage from "@/assets/images/shipon-transparent.png";
-import { FaDownload } from "react-icons/fa6";
+import { TypeAnimation } from "react-type-animation";
 import DownloadResume from "../Shared/DownloadResume";
 
 const Hero = () => {
@@ -18,13 +18,35 @@ const Hero = () => {
         />
       </div>
       <div className="container px-6 lg:px-8 h-full mx-auto py-32 lg:py-0 flex flex-col lg:flex-row justify-between items-center text-white">
-        <div className="lg:w-1/2 w-full md:space-y-6 space-y-3">
+        <div className="lg:w-1/2 w-full md:space-y-5 space-y-3">
           <div>
-            <h2 className="uppercase font-bold md:text-6xl text-3xl">HI, I&apos;M</h2>
+            <h2 className="uppercase font-bold md:text-6xl text-3xl">
+              HI, I&apos;M
+            </h2>
             <h2 className="uppercase font-bold md:text-6xl text-3xl text-primary-main">
               Shipon Irfan!
             </h2>
-            <h2 className="uppercase font-bold md:text-4xl text-xl">Frontend Developer</h2>
+            <TypeAnimation
+              sequence={[
+                "Creative Frontend Developer",
+                1500,
+                "Digital Experience Engineer",
+                1500,
+                "Web Design Wizard",
+                1500,
+                "Code Enthusiast",
+                1500,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{
+                fontSize: "2.5em",
+                display: "inline-block",
+                textTransform: "uppercase",
+                fontWeight: "bold",
+              }}
+              repeat={Infinity}
+            />
           </div>
           <p className="md:pr-20">
             Dedicated MERN Stack Developer, Passionate about Front-End,
@@ -32,12 +54,12 @@ const Hero = () => {
             Solutions 💻✨
           </p>
           <div>
-            <DownloadResume/>
+            <DownloadResume />
           </div>
         </div>
 
         <div className="xl:w-[650px] lg:w-[500px]">
-          <Image src={heroImage}  alt="shipon irfan" />
+          <Image src={heroImage} alt="shipon irfan" />
         </div>
       </div>
     </div>
